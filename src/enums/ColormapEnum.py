f842a5aa6af591d509d8d76eb244b7105f4d488f
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-01-17 17:22:15 -0600

    add "None" colormap

--- src/enums/ColormapEnum.py
+++ src/enums/ColormapEnum.py
@@ -1,14 +1,15 @@
 from enum import Enum
 
 class Colormap(Enum):
-    JET = 0
-    HOT = 1
-    MAGMA = 2
-    INFERNO = 3
-    PLASMA = 4
-    BONE = 5
-    SPRING = 6
-    AUTUMN = 7
-    VIRIDIS = 8
-    PARULA = 9
-    INV_RAINBOW = 10
+    NONE = 0
+    JET = 1
+    HOT = 2
+    MAGMA = 3
+    INFERNO = 4
+    PLASMA = 5
+    BONE = 6
+    SPRING = 7
+    AUTUMN = 8
+    VIRIDIS = 9
+    PARULA = 10
+    INV_RAINBOW = 11
