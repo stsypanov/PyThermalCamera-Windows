42542714dd17e94be72269fdb81a3bff6a36fe99
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-01-17 17:10:30 -0600

    add enum for colormap

---
+++ src/enums/ColormapEnum.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class Colormap(Enum):
+    JET = 0
+    HOT = 1
+    MAGMA = 2
+    INFERNO = 3
+    PLASMA = 4
+    BONE = 5
+    SPRING = 6
+    AUTUMN = 7
+    VIRIDIS = 8
+    PARULA = 9
+    INV_RAINBOW = 10
